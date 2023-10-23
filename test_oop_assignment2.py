00f5ca4ded291b4069ebe235279435f25c243d42
Roger <ostry002@mymail.unisa.edu.au>
2023-10-23 10:30:31 +1030

    Adding a file header which contains my details

--- test_oop_assignment2.py
+++ test_oop_assignment2.py
@@ -1,2 +1,11 @@
+'''
+File: ostry002_assignment2_oop.py
+Description: Implementing the behaviour of an alchemist who is owned by a laboratory
+Author: Roger Osterdahl
+StudentID: 110410137
+EmailID: ostry002
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
+
 class Alchemist:
     pass
