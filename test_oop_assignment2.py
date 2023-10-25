14c50f99acf9215ffd6cfc06f999b32d1976f9f4
Roger <ostry002@mymail.unisa.edu.au>
2023-10-25 15:38:59 +1030

    Embedding testing methods within the test file

--- test_oop_assignment2.py
+++ test_oop_assignment2.py
@@ -7,5 +7,143 @@ EmailID: ostry002
 This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 
-class Alchemist:
-    pass
+import unittest
+
+
+class Alchemist(unittest.TestCase):
+    def __init__(self, attack, strength, defense, magic, ranged, necromancy, laboratory, recipes):
+        self.__attack = attack
+        self.__strength = strength
+        self.__defense = defense
+        self.__magic = magic 
+        self.__ranged = ranged 
+        self.__necromancy = necromancy
+        self.__laboratory = laboratory
+        self.__recipes = []
+    
+    def testGetLaboratory():
+        return Laboratory
+        testGetLaboratory.assertRaises(Laboratory)
+        
+
+    def testGetReceipes(self):
+        return self.__recipes
+    
+    def testMixPotion(self, recipe):
+        self.__laboratory.mixPotion(recipe, self)
+
+    def testDrinkPotion(self, potion):
+        pass
+
+    def testCollectReagent(self, reagent, amount):
+        self.__laboratory.addReagent(reagent, amount)
+    
+    def testRefineReagents(self):
+        pass
+
+    
+
+class Laboratory: 
+    def __init__(self, potions, herbs, catalysts):
+        self.__potions = potions 
+        self.__herbs = herbs 
+        self.__catalysts = catalysts
+
+    def mixPotion(self, name, type, stat, primaryIngredient, secondaryIngredient):
+        pass
+
+    def addReagent(self, reagent, amount):
+        pass
+
+class Potion: 
+    def __init__(self, name, stat, boost):
+        self.__name = name
+        self.__stat = stat 
+        self.__boost = boost
+    
+    def calculateBoost(self):
+        pass
+
+    def getName(self):
+        return self.__name
+    
+    def getStat(self):
+        return self.__stat
+    
+    def getBoost(self):
+        return self.__boost
+    
+    def setBoost(self):
+        pass
+
+class Reagent:
+    def __init__(self, name, potency):
+        self.__name = name
+        self.__potency = potency 
+    
+    def refine(self):
+        pass
+
+    def getName(self):
+        return self.__name
+    
+    def getPotency(self):
+        return self.__potency 
+    
+    def setPotency(self):
+        pass 
+
+class SuperPotion(Potion):
+    def __init__(self, name, stat, boost, herb, catalyst):
+        super().__init__(name, stat, boost)
+        self.__herb = herb
+        self.__catalyst = catalyst 
+    
+    def calculateBoost(self):
+        return super().calculateBoost()
+    
+    def getHerb(self):
+        return self.__herb 
+    
+    def getCatalyst(self):
+        return self.__catalyst 
+
+class ExtremePotion(Potion):
+    def __init__(self, name, stat, boost, reagent, potion):
+        super().__init__(name, stat, boost)
+        self.__reagent = reagent 
+        self.__potion = potion 
+
+    def calculateBoost(self):
+        return super().calculateBoost()
+    
+    def getReagent(Reagent):
+        return Reagent
+    
+    def getPotion(Potion):
+        return Potion
+
+class Herb(Reagent):
+    def __init__(self, name, potency, grimy):
+        super().__init__(name, potency)
+        self.__grimy = grimy 
+
+    def refine(self):
+        return super().refine()
+
+    def getGrimy(self):
+        return self.__grimy 
+    
+    def setGrimy(self):
+        pass
+
+class Catalyst(Reagent):
+    def __init__(self, name, potency, quality):
+        super().__init__(name, potency)
+        self.__quality = quality
+
+    def refine(self):
+        return super().refine()
+    
+    def getQuality(self):
+        return self.__quality 
